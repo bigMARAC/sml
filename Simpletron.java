6251032935482865f384bc7635c57fb9dbf39a16
bigmarac <marcos.length@gmail.com>
2024-04-23 11:10:18 -0300

    refactor

--- Simpletron.java
+++ Simpletron.java
@@ -21,113 +21,123 @@ public class Simpletron {
         accumulator = 0;
     }
     
-    public void load() {
+    public void run() {
+        if (load()) {
+            execute();
+        }
+        getMemory();
+        input.close();
+    }
+    
+    private Boolean load() {
         Integer count = 0;
         Integer word = 0;
         
         do {
             System.out.printf("%02d ? ", count);
             word = input.nextInt();
-            memory.add(count++, word);
+            if ((word >= -9999 && word <= +9999) || word.equals(-99999)) {
+                memory.add(count++, word);
+            }
         } while (!word.equals(-99999) && count < 100);
         
-        System.out.println("*** Program loading completed ***\n*** Program execution begins ***");
+        System.out.println("*** Program loading completed ***");
+        System.out.println("*** Program execution begins ***");
+        return count == 1 ? false : true;
     }
     
-    public void run() {
-        load();
-        execute();
-        getMemory();
-        input.close();
-    }
-    
-    private boolean execute() {
+    private void execute() {
         Boolean stop = false;
         
-        while(!stop) {
+        while (!stop) {
             instructionRegister = memory.get(instructionCounter);
             operationCode = instructionRegister / 100;
             operand = instructionRegister % 100;
-            try {
-                stop = operate();
-                instructionCounter++;
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
+            stop = operate();
+            instructionCounter++;
         }
-        
-        return true;
     }
     
-    private Boolean operate() throws Exception {
-        switch (operationCode) {
-            case 10:
+    private Boolean operate() {
+        switch(operationCode) {
+            case 10: // READ
                 System.out.println("Enter an integer: ");
                 memory.add(operand, input.nextInt());
                 memory.remove(operand + 1);
                 break;
-                // return "READ";
-            case 11:
+            case 11: // WRITE
                 System.out.println(memory.get(operand));
                 break;
-                // return "WRITE";
-            case 20:
+            case 20: // LOAD
                 accumulator = memory.get(operand);
                 break;
-                // return "LOAD";
-            case 21:
+            case 21: // STORE
                 memory.add(operand, accumulator);
+                memory.remove(operand + 1);
                 break;
-                // return "STORE";
-            case 30:
+            case 30: // ADD
                 accumulator += memory.get(operand);
+                checkIsValid(accumulator);
                 break;
-                // return "ADD";
-            case 31:
+            case 31: // SUBTRACT
                 accumulator -= memory.get(operand);
+                checkIsValid(accumulator);
                 break;
-                // return "SUBTRACT";
-            case 32:
+            case 32: // DIVIDE
+                if (memory.get(operand) == 0) {
+                    System.out.println("*** Attempt to divide by zero ***");
+                    System.out.println("*** Simpletron execution abnormally terminated ***");
+                    getMemory();
+                    System.exit(1);
+                    return false;
+                }
                 accumulator /= memory.get(operand);
+                checkIsValid(accumulator);
                 break;
-                // return "DIVIDE";
-            case 33:
+            case 33: // MULTIPLY
                 accumulator *= memory.get(operand);
+                checkIsValid(accumulator);
                 break;
-                // return "MULTIPLY";
-            case 40:
+            case 40: // BRANCH
                 instructionCounter = operand - 1;
                 break;
-                // return "BRANCH";
-            case 41:
+            case 41: // BRANCHNEG
                 instructionCounter = accumulator < 0 ? operand - 1 : instructionCounter;
                 break;
-                // return "BRANCHNEG";
-            case 42:
+            case 42: // BRANCHZERO
                 instructionCounter = accumulator == 0 ? operand - 1 : instructionCounter;
                 break;
-                // return "BRANCHZERO";
-            case 43:
+            case 43: // HALT
                 System.out.println("*** Simpletron execution terminated ***");
                 return true;
-                // return "HALT";
             default:
-                throw new Exception("Morreu");
+                System.out.println("*** Invalid Operation Code ***");
+                System.exit(1);
         }
         
         return false;
     }
     
-    public ArrayList<Integer> getMemory() {
-        System.out.println("REGISTERS:");
-        System.out.printf("accumulator: %d\n", accumulator);
-        System.out.printf("instructionCounter: %02d\n", instructionCounter);
-        System.out.printf("instructionRegister: +%d\n", instructionRegister);
-        System.out.printf("operationCode: %02d\n", operationCode);
-        System.out.printf("operand: %02d\n", operand);
-        System.out.println("MEMORY:");
-        System.out.println(this.memory);
-        return this.memory;
+    private Boolean checkIsValid(Integer number) {
+        if (number >= -9999 && number <= +9999) {
+            return true;
+        }
+        
+        System.out.println("\n*** Invalid Arithmetic Operation ***");
+        System.out.println("*** Simpletron execution abnormally terminated ***");
+        getMemory();
+        System.exit(1);
+        return false;
+    }
+    
+    private void getMemory() {
+        System.out.printf("\nREGISTERS:");
+        System.out.printf("accumulator %d\n", accumulator);
+        System.out.printf("instructionCounter %02d\n", instructionCounter);
+        System.out.printf("instructionRegister +%02d\n", instructionRegister);
+        System.out.printf("operationCode %02d\n", operationCode);
+        System.out.printf("operand %02d\n", operand);
+        System.out.println("MEMORY: ");
+        System.out.println(memory);
     }
 }
